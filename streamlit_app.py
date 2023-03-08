b5fe6eb01530e4c104469b433b79bc90da520695
ahalamora1981 <96462464+ahalamora1981@users.noreply.github.com>
2023-03-08 08:37:55 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,19 +7,19 @@ import time
 st.set_page_config(page_title="My Streamlit App")
 
 # Add a header
-st.header("Welcome to my ChatGPT 3.5!")
+st.header("欢迎使用 GPT 3.5!")
 
 # Add some text
-st.write("This is some sample text.")
+st.write("Powered by Johnny Tao")
 
 # Add a sidebar
-option = st.sidebar.selectbox("Select an option", ("one", "two", "three"))
+option = st.sidebar.selectbox("选择模型", ("GPT 3.5", "Davici-003"))
 
 # Add a text input
-text_input = st.text_input("Enter some text")
+text_input = st.text_input("我:")
 
 # Add a submit button
-bnt = st.button("Submit")
+bnt = st.button("发送")
 
 
 API_KEY = st.secrets["openai_api_key"]
@@ -41,7 +41,7 @@ while bnt:
 
     new_output = completion.choices[0].message.to_dict()
     msg.append(new_output)
-    st.write("AI Response:")
+    st.write("GPT 3.5:")
     st.write(new_output["content"])
 
     print(new_output)
